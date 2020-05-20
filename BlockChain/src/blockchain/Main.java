037c952f1f0a4688dcc8ddfcbac3b6f36cc536bd
Aleonor1 <adrianaleodor@gmail.com>
2020-05-20 21:44:09 +0300

    bug  fix main executor

--- BlockChain/src/blockchain/Main.java
+++ BlockChain/src/blockchain/Main.java
@@ -1,5 +1,6 @@
 package blockchain;
  
+import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -9,22 +10,25 @@ public class Main {
     public static void main(String[] args) {
         try {
             int threadCount = Runtime.getRuntime().availableProcessors();
-            ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+            ExecutorService executor = Executors.newFixedThreadPool(5);
             BlockChain blockChain = new BlockChain();
             for (int i = 0; i < threadCount / 2; i++) {
                 executor.submit(new Miner(blockChain, i));
                 executor.submit(new Chatter(blockChain, "Chatter " + i));
             }
-            System.out.println(executor.getClass().getName());
+
             executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.SECONDS);
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+           // if (blockChain.blocks() instanceof Collection) {
+             //   System.out.println( ((Collection<?>) blockChain.blocks()).size());
+            //}
             int i = 0;
             for (Block block: blockChain.blocks()) {
-                if (i++ == 6) {
-                    break;
-                }
                 System.out.println(block);
                 System.out.println();
+                if (i++ == 4) {
+                    break;
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
